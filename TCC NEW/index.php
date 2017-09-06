3ce94e171bae0d6aaad3a43bdc2be548a9509148
Kevin Katzer <kevinka999@gmail.com>
2017-09-05 21:41:38 -0300

    v.1

---
+++ TCC NEW/index.php
@@ -0,0 +1,68 @@
+﻿<?php
+	/* index.php */
+	include  ('./cabecalho_nav.php');
+?>
+
+		<article>
+		
+            <div class="container-fluid colorblue">
+				<div class="container">
+					<div class="row">
+						<h1 class="textwhite text-center">SportTeams</h1>
+						<p class="textwhite text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+						quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+						
+						<p class="text-center"><a href="#"><i class="fa fa-facebook-square textwhite" style="font-size:50px !important;"></i></a>;&nbsp;;&nbsp;;&nbsp;
+						<a href="#"><i class="fa fa-twitter-square textwhite" style="font-size:50px !important;"></i></a>
+						</p>
+						
+					</div>
+				</div>
+			</div>
+			
+            <br/>
+			
+            <div class="container">
+                <div class="row">
+                  <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail thumbnail-inverse">
+                      <img src="img/voleilogo.png" alt="Voleibol">
+                      <div class="caption">
+                        <h3 class="textwhite text-center">VOLEIBOL</h3>
+                        <p class="textwhite2 text-center">Se você procura um time ou jogadores de voleibol, aqui é seu lugar.</p>
+						<br/>
+						<p class="text-center"><a href="#" class="btn btn-default" role="button">Jogadores</a> <a href="#" class="btn btn-default" role="button">Times</a></p>
+                      </div>
+                    </div>
+                  </div>
+				  
+				  <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail thumbnail-inverse">
+                      <img src="img/futebollogo.png" alt="Futebol">
+                      <div class="caption">
+                        <h3 class="textwhite text-center">FUTEBOL</h3>
+                        <p class="textwhite2 text-center">Se você procura um time ou jogadores de futebol, aqui é seu lugar.</p>
+						<br/>
+						<p class="text-center"><a href="#" class="btn btn-default" role="button">Jogadores</a> <a href="#" class="btn btn-default" role="button">Times</a></p>
+                      </div>
+                    </div>
+                  </div>
+				  
+				  <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail thumbnail-inverse">
+                      <img src="img/basquetelogo.png" alt="Basquete">
+                      <div class="caption">
+                        <h3 class="textwhite text-center">BASQUETE</h3>
+                        <p class="textwhite2 text-center">Se você procura um time ou jogadores de basquete, aqui é seu lugar.</p>
+						<br/>
+						<p class="text-center"><a href="#" class="btn btn-default" role="button">Jogadores</a> <a href="#" class="btn btn-default" role="button">Times</a></p>
+                      </div>
+                    </div>
+                  </div>
+
+            </div>
+		</article>
+		
+<?php
+	Include  ('./rodape_assinatura.php');
+?>
