abb0d638ba21e39a8a8eafa81a4198250c505227
Maria <maria.piontkowski@hotmail.com>
2017-10-18 17:27:39 -0200

    First Commit

--- TCC NEW/rodape_assinatura.php
+++
@@ -1,9 +0,0 @@
-	<footer>
-			<p class="text-center textwhiteborderblack">© SportsTeams - Todos direitos reservados.</p>
-	</footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-    
-</html>
