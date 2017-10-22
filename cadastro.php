df7d623f4d6bb4375b53ef69e3839c812ddf3435
MariaPiontkowski <maria.piontkowski@hotmail.com>
2017-10-22 20:30:23 -0200

    Cadastro Usuário

--- cadastro.php
+++ cadastro.php
@@ -68,7 +68,8 @@ include('./cabecalho_cadastro.php');
 
                 //Permite apenas numeros
                 if ((event.shiftKey || (event.keyCode < 48 || event.keyCode > 57)) && (event.keyCode < 96 || event.keyCode > 105)) {
-                    event.preventDefault();                }
+                    event.preventDefault();
+                }
             });
 
 
